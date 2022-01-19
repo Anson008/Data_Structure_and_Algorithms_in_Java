01a956f58afbf690f01b13a1f805488d9c3a0fc9
Xingchen Zhao <xczhao007@gmail.com>
2022-01-19 15:06:30 -0600

    Change "Queue" to "IQueue" to indicate it is an interface

--- src/Chapter06/IQueue.java
+++ src/Chapter06/IQueue.java
@@ -1,6 +1,6 @@
 package Chapter06;
 
-public interface Queue<E> {
+public interface IQueue<E> {
     int size();  // Return the number of elements in the queue.
     boolean isEmpty();
     void enqueue(E e);
